aafe827c515646a0afd0142a365567bd1d09f2fd
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-04-04 14:13:35 -0500

    Simple function that prints to the browser console when called

--- overly-excited.js
+++ overly-excited.js
@@ -1,2 +1,9 @@
 // For this exercise, you need to create a single JavaScript function named addExcitement that will use console.log() to print out a sentence to the browser console.
 
+function addExcitement(message) {
+    console.log(message);
+    // document.write(message);
+    // //return yourMessage;
+}
+
+addExcitement("Hello World");
